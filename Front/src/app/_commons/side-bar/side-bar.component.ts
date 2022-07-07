04e13269b7d2cdf30719c5ed4e59fe0a7ee8d5cb
Marie <Marue59@hotmail.fr>
2022-07-07 12:16:02 +0200

    feat: configuration des themes et sousthemes :sparkles:

--- Front/src/app/_commons/side-bar/side-bar.component.ts
+++ Front/src/app/_commons/side-bar/side-bar.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 
 import { SousThemeService } from 'src/app/services/sousTheme/sous-theme.service';
 import SousTheme from 'src/app/models/sousTheme.models';
+import { ThemeService } from 'src/app/services/theme/theme.service';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
@@ -11,10 +13,20 @@ import SousTheme from 'src/app/models/sousTheme.models';
 export class SideBarComponent implements OnInit {
   @Input()
   sousTheme!: SousTheme;
+
+  themes!: any;
+  subscription!: Subscription;
+
   constructor(
     private sousThemeService: SousThemeService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private themeService: ThemeService
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.subscription = this.themeService.currentTheme.subscribe((themes) => {
+      this.themes = themes;
+      console.log(this.themes);
+    });
+  }
 }
--- Front/src/app/pages/theme/theme-list/theme-list.component.ts
+++ Front/src/app/pages/theme/theme-list/theme-list.component.ts
@@ -9,5 +9,43 @@ export class ThemeListComponent implements OnInit {
   @Input() themes: any;
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log(this.themes);
+  }
+
+  /*
+  {
+    "id": 3,
+    "nom": "Gestion de proojet",
+    "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+    ,"sous-theme" : [{
+      "id": 1,
+      "nom": "JAva",
+      "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+    },
+    {
+      "id": 2,
+      "nom": "Js",
+      "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+
+    }
+  ]
+  },
+  {
+    "id": 4,
+    "nom": "Dev",
+    "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+    ,"sous-theme" : [{
+      "id": 1,
+      "nom": "JAva",
+      "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+    },
+    {
+      "id": 2,
+      "nom": "Js",
+      "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+
+    }
+  ]
+  }*/
 }
