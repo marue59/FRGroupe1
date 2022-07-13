51f07b10b4573ade20d3b31cfbd32df7777b6afa
Thomas Barré <thomasbarre07@gmail.com>
2022-07-13 11:40:33 +0200

    formulaire-reactif-v6

--- Back/src/main/java/FR/Groupe1/ITTraining/controller/UtilisateurController.java
+++ Back/src/main/java/FR/Groupe1/ITTraining/controller/UtilisateurController.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/utilisateurs")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin()
 public class UtilisateurController {
 
     @Autowired
